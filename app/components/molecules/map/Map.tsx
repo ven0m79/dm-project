0013d3094b075355fe5a22a2747162fdbd51ec14
Max Pashynov <maxpachinov@gmail.com>
2024-01-17 12:38:10 +0200

    feat: added transforms for specific map regions

--- app/components/molecules/map/Map.tsx
+++ app/components/molecules/map/Map.tsx
@@ -6,7 +6,17 @@ const MapOfUkraine = () => {
   return (
     <svg width="700px" height="480px" viewBox="0 0 700 480">
       {Map_Data.map((el, i) => (
-        <path key={i} d={el.path} fill="orange" />
+        <path
+          key={i}
+          d={el.path}
+          fill="orange"
+          style={{
+            transform:
+              el.id === 12 || el.id === 13 || el.id === 15
+                ? "translate(-3.36%, 2.4%)"
+                : undefined,
+          }}
+        />
       ))}
     </svg>
   );
