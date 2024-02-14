072d6de606d30411fcf229c4e0e26ddb59bfe970
ven0m <yermolenko79@gmail.com>
2024-02-14 14:11:13 +0200

    dm-next-service

--- app/[locale]/services/page.tsx
+++ app/[locale]/services/page.tsx
@@ -9,8 +9,14 @@ import classNames from "classnames";
 import { MainLayout } from "@app/[locale]/components/templates";
 import { unstable_setRequestLocale } from "next-intl/server";
 
+import { useTranslations } from 'next-intl';
+import { Link } from "config";
+import Image from 'next/image';
+import { title } from "process";
+
 const ServicesPage = ({ params: { locale } }: { params: { locale: string } }) => {
   //unstable_setRequestLocale(locale);
+  const t = useTranslations('ServicePage');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
@@ -28,35 +34,35 @@ const ServicesPage = ({ params: { locale } }: { params: { locale: string } }) =>
     <MainLayout>
       <div className={classNames("flex flex-1 flex-row self-center", styles.main)}>
         <div className={classNames("text-gray-900 flex flex-1 flex-col justify-normal items-start w-full mt-8", styles.leftContainer)}>
-        <div className=" text-black self-center h-96 text-5xl">Сервісне обслуговування</div>
+        <div className={styles.sendUsMessage}>{t('title')}</div>
         </div>
         <div className="text-gray-900 justify-center items-start pr-5">
           <form onSubmit={handleSubmit} className={styles.container}>
-            <div className={styles.sendUsMessage}>Contact with Service Center</div>
+            <div className={styles.sendUsMessage}>{t('contact-form-title')}</div>
             <input
               className={classNames("h-12", styles.form)}
-              placeholder="Name"
+              placeholder={t('contact-form-name')}
               id="name"
               type="text"
               onChange={e => setName(e.target.value)}
             /><br />
             <input
               className={classNames("h-12", styles.form)}
-              placeholder="Email"
+              placeholder={t('contact-form-email')}
               id="email"
               type="email"
               onChange={e => setEmail(e.target.value)}
             /><br />
             <textarea
               className={classNames("h-40", styles.form)}
-              placeholder="Write Your Message here..."
+              placeholder={t('contact-form-message')}
               id="message"
               itemType="text"
               onChange={e => setMessage(e.target.value)}
             /><br />
             <button 
             className={styles.yerSubmit}
-            type="submit">SEND MESSAGE</button>
+            type="submit">{t('contact-form-submit')}</button>
           </form>
         </div>
       </div>
