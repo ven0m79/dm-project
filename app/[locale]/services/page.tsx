f1eeb1963d3d491074f63fe46ede89d1b0638e5d
ven0m <yermolenko79@gmail.com>
2024-02-15 21:11:34 +0200

    dm-next-service-фдд

--- app/[locale]/services/page.tsx
+++ app/[locale]/services/page.tsx
@@ -6,6 +6,8 @@ import Head from 'next/head';
 import styles from './Service.module.css';
 import classNames from "classnames";
 
+import imgSrc from './photos/svarka_servis.jpg';
+
 import { MainLayout } from "@app/[locale]/components/templates";
 import { unstable_setRequestLocale } from "next-intl/server";
 
@@ -32,39 +34,65 @@ const ServicesPage = ({ params: { locale } }: { params: { locale: string } }) =>
   };
   return (
     <MainLayout>
-      <div className={classNames("flex flex-1 flex-row self-center", styles.main)}>
-        <div className={classNames("text-gray-900 flex flex-1 flex-col justify-normal items-start w-full mt-8", styles.leftContainer)}>
-        <div className={styles.sendUsMessage}>{t('title')}</div>
+      <div className={classNames("flex flex-1 flex-col self-center", styles.main)}>
+        <div className={styles.sendUsMessage}>
+          {t('title')}
         </div>
-        <div className="text-gray-900 justify-center items-start pr-5">
-          <form onSubmit={handleSubmit} className={styles.container}>
-            <div className={styles.sendUsMessage}>{t('contact-form-title')}</div>
-            <input
-              className={classNames("h-12", styles.form)}
-              placeholder={t('contact-form-name')}
-              id="name"
-              type="text"
-              onChange={e => setName(e.target.value)}
-            /><br />
-            <input
-              className={classNames("h-12", styles.form)}
-              placeholder={t('contact-form-email')}
-              id="email"
-              type="email"
-              onChange={e => setEmail(e.target.value)}
-            /><br />
-            <textarea
-              className={classNames("h-40", styles.form)}
-              placeholder={t('contact-form-message')}
-              id="message"
-              itemType="text"
-              onChange={e => setMessage(e.target.value)}
-            /><br />
-            <button 
-            className={styles.yerSubmit}
-            type="submit">{t('contact-form-submit')}</button>
-          </form>
+        <div className={classNames("text-gray-900 flex flex-1 flex-row justify-normal items-start w-full")}>
+          <div className={classNames("text-gray-900 flex flex-1 flex-col justify-normal items-start w-full")}>
+            <div className={classNames("text-gray-900 justify-normal items-start indent-4 w-full mt-8", styles.leftContainer)}>
+              <Image 
+              className="float-left m-5"
+              src={imgSrc}
+              width={300} 
+              height={200}
+              alt={"photo"}>
+
+              </Image>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, voluptate numquam. Facere magnam itaque ex fugit molestias ducimus aperiam at, id modi cumque fugiat rerum impedit similique quod expedita ea.
+              Alias adipisci optio consequatur? Voluptas nisi, facere ipsum blanditiis atque, officia adipisci aliquid rem facilis amet saepe numquam vero provident, laudantium beatae incidunt consectetur qui cum dolores culpa deleniti maiores.
+              Eligendi dolorem quam saepe at dolores rem neque pariatur totam eius libero eaque, eum quod accusamus illo sint nihil ut! Alias corporis odit, consectetur distinctio fuga non quasi quam enim.</div>
+            <div className={classNames("text-gray-900 justify-normal items-start indent-4 w-full mt-8", styles.leftContainer)}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, voluptate numquam. Facere magnam itaque ex fugit molestias ducimus aperiam at, id modi cumque fugiat rerum impedit similique quod expedita ea.
+              Alias adipisci optio consequatur? Voluptas nisi, facere ipsum blanditiis atque, officia adipisci aliquid rem facilis amet saepe numquam vero provident, laudantium beatae incidunt consectetur qui cum dolores culpa deleniti maiores.
+              Eligendi dolorem quam saepe at dolores rem neque pariatur totam eius libero eaque, eum quod accusamus illo sint nihil ut! Alias corporis odit, consectetur distinctio fuga non quasi quam enim.</div>
+            <div className={classNames("text-gray-900 justify-normal items-start indent-4 w-full mt-8", styles.leftContainer)}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto, voluptate numquam. Facere magnam itaque ex fugit molestias ducimus aperiam at, id modi cumque fugiat rerum impedit similique quod expedita ea.
+              Alias adipisci optio consequatur? Voluptas nisi, facere ipsum blanditiis atque, officia adipisci aliquid rem facilis amet saepe numquam vero provident, laudantium beatae incidunt consectetur qui cum dolores culpa deleniti maiores.
+              Eligendi dolorem quam saepe at dolores rem neque pariatur totam eius libero eaque, eum quod accusamus illo sint nihil ut! Alias corporis odit, consectetur distinctio fuga non quasi quam enim.</div>
+          </div>
+          <div>
+            <form onSubmit={handleSubmit} className={styles.container}>
+              <div className={styles.sendUsMessage}>{t('contact-form-title')}</div>
+              <input
+                className={classNames("h-12", styles.form)}
+                placeholder={t('contact-form-name')}
+                id="name"
+                type="text"
+                onChange={e => setName(e.target.value)}
+              /><br />
+              <input
+                className={classNames("h-12", styles.form)}
+                placeholder={t('contact-form-email')}
+                id="email"
+                type="email"
+                onChange={e => setEmail(e.target.value)}
+              /><br />
+              <textarea
+                className={classNames("h-40", styles.form)}
+                placeholder={t('contact-form-message')}
+                id="message"
+                itemType="text"
+                onChange={e => setMessage(e.target.value)}
+              /><br />
+              <button
+                className={styles.yerSubmit}
+                type="submit">{t('contact-form-submit')}</button>
+            </form>
+
+          </div>
         </div>
+
       </div>
     </MainLayout>
   );
