71d93c43bb7bd9262ea12f975eedda77b478e9b0
ven0m <yermolenko79@gmail.com>
2024-02-23 16:25:06 +0200

    dm-next-API

---
+++ pages/api/get-products.tsx
@@ -0,0 +1,36 @@
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+
+const api = new WooCommerceRestApi({
+    url: process.env.NEXT_PUBLIC_WORDPRESS_RITE_URL,
+    consumerKey: process.env.WC_CONSUMER_KEY,
+    consumerSecret: process.env.WC_CONSUMER_SECRET,
+    version: "wc/v3"
+});
+
+export default async function handler(req, res) {
+    const responseData = {
+        success: false,
+        products: [],
+        error: "Не підсосало"
+
+    }
+
+    try {
+        const { data } = await api.get(
+            'products',
+            {
+                per_page: 10
+            },
+
+        );
+
+        responseData.success = true;
+        responseData.products = data;
+
+        res.json (responseData)
+    } catch (error) {
+        responseData.error = error.message;
+        res.status(500).json(responseData);
+    }
+
+}
