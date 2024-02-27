7337199fc28c3a5c4b6175a306ec193a6840b798
ven0m <yermolenko79@gmail.com>
2024-02-27 09:14:37 +0200

    dm-next-font-renew

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)'],
+        sans: ['var(--font-roboto)'],
         mono: ['var(--font-roboto-mono)'],
       },
     },
