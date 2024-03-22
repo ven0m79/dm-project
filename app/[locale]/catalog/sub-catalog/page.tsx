8277264c4a2a0479c5b942f50bcef63d67bf8f8a
Max Pashynov <maxpachinov@gmail.com>
2024-03-22 15:41:42 +0200

    added loader for sidebar items

--- app/[locale]/catalog/sub-catalog/page.tsx
+++ app/[locale]/catalog/sub-catalog/page.tsx
@@ -17,14 +17,11 @@ const SubCatalog = ({ params: { locale } }: { params: { locale: string } }) => {
   // треба буде формувати структуру, у якій значення із попередньої сторінки буде відповідати вибранному sidebar елементу
   const searchParams = useSearchParams();
   const selectedCategory = searchParams?.get("category");
-  console.log({ selectedCategory });
 
   const [categories, setCategories] = useState<TransformedCategoriesType[]>([]);
   const [selectedProducts, setSelectedProducts] = useState<
     SingleProductDetails[]
   >([]);
-  console.log({ categories });
-
 
   const getData = useCallback(async () => {
     try {
@@ -48,77 +45,76 @@ const SubCatalog = ({ params: { locale } }: { params: { locale: string } }) => {
   }, [getData]);
 
   return (
-    <MainLayout>
-      <div
-        className={classNames(
-          "flex flex-1 flex-row justify-between self-center",
-          styles.subCatalog,
-        )}
-      >
-        <div className={classNames("mt-4", styles.subMenu)}>
-          <Suspense>
+    <Suspense fallback="Loading">
+      <MainLayout>
+        <div
+          className={classNames(
+            "flex flex-1 flex-row justify-between self-center",
+            styles.subCatalog,
+          )}
+        >
+          <div className={classNames("mt-4", styles.subMenu)}>
             <Sidebar
               items={[categories?.[1] || []]}
+              categoryTag={selectedCategory}
               setSelectedProducts={setSelectedProducts}
             />
-          </Suspense>
-          <div className={classNames("", styles.subMenuDash)}></div>
-        </div>
+            <div className={classNames("", styles.subMenuDash)}></div>
+          </div>
 
-        <div className="flex flex-wrap justify-around self-center mt-4 mb-4 mx-1 w-full">
-          {selectedProducts && selectedProducts.length ? (
-            selectedProducts.map((el) => {
-              return (
-                <div
-                  key={el.id}
-                  className={classNames("mx-5", styles.headSubCatalogBlock)}
-                >
-                  <div className="">
-                    <Link
-                      locale={locale}
-                      key={el.id}
-                      href={`/catalog/sub-catalog/product/${el.id}`}
-                    >
-                      <div
-                        className={classNames(
-                          "cursor-pointer",
-                          styles.headSubCatalogPhoto,
-                        )}
+          <div className="flex flex-wrap justify-around self-center mt-4 mb-4 mx-1 w-full">
+            {selectedProducts && selectedProducts.length ? (
+              selectedProducts.map((el) => {
+                return (
+                  <div
+                    key={el.id}
+                    className={classNames("mx-5", styles.headSubCatalogBlock)}
+                  >
+                    <div className="">
+                      <Link
+                        locale={locale}
+                        key={el.id}
+                        href={`/catalog/sub-catalog/product/${el.id}`}
                       >
-                        <img
-                          src={el.images[0].src}
-                          alt={el.images[0].alt}
-                          width={310}
-                          height={360}
-                        />
-                      </div>
+                        <div
+                          className={classNames(
+                            "cursor-pointer",
+                            styles.headSubCatalogPhoto,
+                          )}
+                        >
+                          <img
+                            src={el.images[0].src}
+                            alt={el.images[0].alt}
+                            width={310}
+                            height={360}
+                          />
+                        </div>
 
-                      <div className="flex justify-center">
-                        <h3 className={classNames("", styles.headSubCatalog)}>
-                          {el.name}
-                        </h3>
-                      </div>
-                    </Link>
+                        <div className="flex justify-center">
+                          <h3 className={classNames("", styles.headSubCatalog)}>
+                            {el.name}
+                          </h3>
+                        </div>
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              );
-            })
-          ) : (
-            <h2 className="text-amber-700">Немає результатів пошуку</h2>
-          )}
-        </div>
+                );
+              })
+            ) : (
+              <h2 className="text-amber-700">Немає результатів пошуку</h2>
+            )}
+          </div>
 
-        <div className={classNames("mt-4", styles.subMenu)}>
-          <Suspense>
+          <div className={classNames("mt-4", styles.subMenu)}>
             <Sidebar
-               items={[categories?.[0] || []]}
+              items={[categories?.[0] || []]}
               setSelectedProducts={setSelectedProducts}
             />
-          </Suspense>
-          <div className={classNames("", styles.subMenuDash)}></div>
+            <div className={classNames("", styles.subMenuDash)}></div>
+          </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </Suspense>
   );
 };
 
--- app/[locale]/components/molecules/leftSidebar/leftSidebar.tsx
+++ app/[locale]/components/molecules/leftSidebar/leftSidebar.tsx
@@ -1,27 +1,42 @@
 "use client";
 
-import { Sidebar as FBSidebar } from "flowbite-react";
-import styles from "../../../catalog/sub-catalog/Sub-catalog.module.css";
-import React, { FC, useCallback, useState } from "react";
 import classNames from "classnames";
+import { Sidebar as FBSidebar } from "flowbite-react";
+import React, { FC, useCallback, useEffect } from "react";
+
+import styles from "../../../catalog/sub-catalog/Sub-catalog.module.css";
 
 import { fetchWooCommerceProductsBasedOnCategory } from "../../../../../utils/woocommerce.setup";
 import { TransformedCategoriesType } from "@app/[locale]/catalog/sub-catalog/helpers";
 
+const LEFT_BAR_PARENT_ID = 50;
+const RIGHT_BAR_PARENT_ID = 55;
+
+const categoriesIdData = {
+  operations: 19,
+  "intensive-therapy": 75,
+  neonathal: 79,
+  sterilization: 83,
+  gaz: 87,
+  furniture: 91,
+  accessories: 95,
+};
+
 type LeftSidebarProps = {
   items: TransformedCategoriesType[];
+  categoryTag?: string | null | keyof typeof categoriesIdData;
   setSelectedProducts?: (v: any[]) => void;
 };
 
-const Sidebar: FC<LeftSidebarProps> = ({ items, setSelectedProducts }) => {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-
+const Sidebar: FC<LeftSidebarProps> = ({
+  items,
+  categoryTag,
+  setSelectedProducts,
+}) => {
   const getCategoryDetails = useCallback(
     async (id: number) => {
       try {
         const data = await fetchWooCommerceProductsBasedOnCategory(id);
-
-        console.log({ data });
 
         if (data) {
           setSelectedProducts?.(data);
@@ -33,8 +48,12 @@ const Sidebar: FC<LeftSidebarProps> = ({ items, setSelectedProducts }) => {
     [setSelectedProducts],
   );
 
-  // оця хуйня робить всю магію з створенням вкладених dropdown options
-  const renderNestedCategories = (category: TransformedCategoriesType) => {
+  const renderNestedCategories = (
+    category: TransformedCategoriesType,
+    level = 0,
+  ) => {
+    const marginLeft = level === 0 || level === 1 ? 0 : level * 10;
+
     return category?.childrens?.length === 0 ? (
       <FBSidebar.Item
         as="div"
@@ -42,21 +61,46 @@ const Sidebar: FC<LeftSidebarProps> = ({ items, setSelectedProducts }) => {
         className={classNames("cursor-pointer", styles.subItem)}
         onClick={() => getCategoryDetails(category.id)}
       >
-        {category.name}
+        <div style={{ marginLeft: marginLeft }}>{category.name}</div>
       </FBSidebar.Item>
     ) : (
       <FBSidebar.Collapse
-      open
+        open={
+          category.id === LEFT_BAR_PARENT_ID ||
+          category.id === RIGHT_BAR_PARENT_ID ||
+          category.id ===
+            categoriesIdData[categoryTag as keyof typeof categoriesIdData]
+        }
         label={category.name}
         key={category.id}
-        className={classNames("", styles.subCollapse)}
+        className={classNames("", styles.subCollapse, {
+          "opacity-0 pointer-events-none mt-[-40px]":
+            category.id === LEFT_BAR_PARENT_ID ||
+            category.id === RIGHT_BAR_PARENT_ID,
+        })}
       >
         {category?.childrens?.length
-          ? category.childrens.map((child) => renderNestedCategories(child))
+          ? category.childrens.map((child) =>
+              renderNestedCategories(child, level + 1),
+            )
           : null}
       </FBSidebar.Collapse>
     );
   };
+
+  useEffect(() => {
+    if (
+      categoriesIdData?.[
+        categoryTag as unknown as keyof typeof categoriesIdData
+      ]
+    ) {
+      getCategoryDetails(
+        categoriesIdData?.[
+          categoryTag as unknown as keyof typeof categoriesIdData
+        ],
+      );
+    }
+  }, [categoryTag, getCategoryDetails]);
 
   return (
     <div
@@ -66,6 +110,9 @@ const Sidebar: FC<LeftSidebarProps> = ({ items, setSelectedProducts }) => {
       )}
     >
       <div className={classNames("mt-4", styles.subMenu)}>
+        {/*Оцю стилізувати*/}
+        <h3 className="text-amber-700">{items?.[0]?.name}</h3>
+
         <FBSidebar aria-label="Catalog" className="">
           <FBSidebar.ItemGroup>
             {items?.map((el) => renderNestedCategories(el))}
