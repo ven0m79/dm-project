00bc25fb7dcf65def9b96241f93e4f1daffbb70a
ven0m <yermolenko79@gmail.com>
2024-06-21 18:50:44 +0300

    home-shares

--- app/[locale]/shares/page.tsx
+++ app/[locale]/shares/page.tsx
@@ -11,13 +11,16 @@ import logo from "./shares-photo/DM-project-logo-transparent.png";
 import linea from "./linea/linea-trans.webp";
 import polaris from "./polaris200/polaris200-trans.webp";
 import polaris2 from "./polaris200-2/polaris200-2-trans.webp";
+import fabiusplusxl from "./fabiusplusxl/Fabius_plus_XL-trans.webp";
+import atlan300 from "./Atlan300/Atlan300.webp"
 
 const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
   unstable_setRequestLocale(locale);
   return (
-    <MainLayout><div
+    <MainLayout>
+      <div
       className={classNames(
-        "flex flex-wrap justify-start mb-5 gap-5 mt-5",
+        "flex flex-wrap mb-5 gap-5 mt-5",
         [styles.catalogContainer],
       )}
     >
@@ -45,14 +48,14 @@ const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
               <span className={classNames("text-black", styles.span)}>Акційна комерційна пропозиція</span>
             </div>
             <div>
-              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Панель Linea</span>
+              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Панель <span className="underline">Linea</span></span>
             </div>
           </div>
           <div className="flex w-[150px] h-[150px] items-center justify-end mr-1">
             <Image
               className={styles.img}
               src={linea}
-              width={225}
+              width={215}
               height={150}
               alt="logo"
             />
@@ -84,7 +87,7 @@ const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
               <span className={classNames("text-black", styles.span)}>Акційна комерційна пропозиція</span>
             </div>
             <div>
-              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Світильник операційний Polaris 200 однокупольний</span>
+              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Світильник операційний <span className="underline">Polaris 200</span> однокупольний</span>
             </div>
           </div>
           <div className="flex w-[150px] h-[150px] items-center justify-end mr-1">
@@ -123,7 +126,7 @@ const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
               <span className={classNames("text-black", styles.span)}>Акційна комерційна пропозиція</span>
             </div>
             <div>
-              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Світильник операційний Polaris 200 двокупольний</span>
+              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Світильник операційний <span className="underline">Polaris 200</span> двокупольний</span>
             </div>
           </div>
           <div className="flex w-[150px] h-[150px] items-center justify-end mr-1">
@@ -138,7 +141,83 @@ const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
         </div>
       </Link>
 
-      
+      <Link
+        href={{
+          pathname: "/shares/linea",
+        }}
+      >
+        <div
+          className={classNames(
+            "flex flex-row items-center rounded-xl justify-between",
+            styles["block-decisions"],
+          )}
+        ><div className="flex w-[200px] h-[150px] bg-[#0d3d61] items-center justify-center rounded-l-lg rounded-r-none">
+            <Image
+              className={styles.img}
+              src={logo}
+              width={100}
+              height={100}
+              alt="logo"
+            />
+          </div>
+          <div className="flex flex-col w-[350px] h-[150px] pl-5 mt-5">
+            <div className="mb-3">
+              <span className={classNames("text-black", styles.span)}>Акційна комерційна пропозиція</span>
+            </div>
+            <div>
+              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Наркозно-дихальний апарат <span className="underline">Fabius plus XL</span> + Монітор пацієнта <span className="underline">Vista 120S</span></span>
+            </div>
+          </div>
+          <div className="flex w-[150px] h-[150px] items-center justify-end mr-1">
+            <Image
+              className={styles.img}
+              src={fabiusplusxl}
+              width={109}
+              height={150}
+              alt="logo"
+            />
+          </div>
+        </div>
+      </Link>
+
+      <Link
+        href={{
+          pathname: "/shares/linea",
+        }}
+      >
+        <div
+          className={classNames(
+            "flex flex-row items-center rounded-xl justify-between",
+            styles["block-decisions"],
+          )}
+        ><div className="flex w-[200px] h-[150px] bg-[#0d3d61] items-center justify-center rounded-l-lg rounded-r-none">
+            <Image
+              className={styles.img}
+              src={logo}
+              width={100}
+              height={100}
+              alt="logo"
+            />
+          </div>
+          <div className="flex flex-col w-[350px] h-[150px] pl-5 mt-5">
+            <div className="mb-3">
+              <span className={classNames("text-black", styles.span)}>Акційна комерційна пропозиція</span>
+            </div>
+            <div>
+              <span className={classNames("text-[#0d3d61] font-bold text-2xl", styles.span)}>Наркозно-дихальний апарат <span className="underline">Atlan A300</span> + Монітор пацієнта <span className="underline">Vista 120S</span></span>
+            </div>
+          </div>
+          <div className="flex w-[150px] h-[150px] items-center justify-end mr-1">
+            <Image
+              className={styles.img}
+              src={atlan300}
+              width={109}
+              height={150}
+              alt="logo"
+            />
+          </div>
+        </div>
+      </Link>
 
     </div>
     </MainLayout>
