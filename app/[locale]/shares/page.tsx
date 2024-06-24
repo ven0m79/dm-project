d0883fa6285e901d78b97a1b606852cbed79cf29
ven0m <yermolenko79@gmail.com>
2024-06-24 22:37:03 +0300

    home-shares-linea

--- app/[locale]/shares/page.tsx
+++ app/[locale]/shares/page.tsx
@@ -12,7 +12,8 @@ import linea from "./linea/linea-trans.webp";
 import polaris from "./polaris200/polaris200-trans.webp";
 import polaris2 from "./polaris200-2/polaris200-2-trans.webp";
 import fabiusplusxl from "./fabiusplusxl/Fabius_plus_XL-trans.webp";
-import atlan from "./atlan300/atlan300.webp"
+//import atlan from "./atlan300/atlan300.webp"
+
 
 const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
   unstable_setRequestLocale(locale);
@@ -210,7 +211,7 @@ const Shares = ({ params: { locale } }: { params: { locale: string } }) => {
           <div className="flex w-[150px] h-[150px] items-center justify-end mr-1">
             <Image
               className={styles.img}
-              src={atlan}
+              src={fabiusplusxl}
               width={109}
               height={150}
               alt="logo"
