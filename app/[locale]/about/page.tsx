d8b4e9436b1a14c9f2543b15056f7207e4472f07
ven0m <yermolenko79@gmail.com>
2024-10-06 18:09:25 +0300

    correct-for-vercel6

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from "react";
+import React, { Suspense } from "react";
 import classNames from "classnames";
 import { MainLayout } from "@app/[locale]/components/templates";
 import styles from "./About.module.css";
@@ -15,86 +15,87 @@ const AboutPage = ({ params: { locale } }: { params: { locale: string } }) => {
   //unstable_setRequestLocale(locale);
   const t = useTranslations('AboutPage');
   return (
-    <MainLayout>
-      <div
-        className={classNames(
-          "flex flex-1 flex-col justify-center items-start",
-          styles["aboutContainer"],
-        )}
-      >
+    <Suspense fallback="Loading">
+      <MainLayout>
         <div
           className={classNames(
-            "flex flex-1 flex-row justify-center items-start w-[1400px]",
+            "flex flex-1 flex-col justify-center items-start",
+            styles["aboutContainer"],
           )}
         >
-          <Image
-            src={vystavka}
-            className="p-5 float-left"
-            width={550}
-            height={750}
-            alt={""}>
-          </Image>
+          <div
+            className={classNames(
+              "flex flex-1 flex-row justify-center items-start w-[1400px]",
+            )}
+          >
+            <Image
+              src={vystavka}
+              className="p-5 float-left"
+              width={550}
+              height={750}
+              alt={""}>
+            </Image>
+
+            <div
+              className={classNames(
+                "flex flex-column justify-center items-start my-3",
+                styles["aboutText"],
+              )}
+            >
+              <div className={classNames("my-2 indent-5 text-justify")}>
+                {t('information1')}
+              </div>
+              <div className={classNames("my-2 indent-5 text-justify")}>
+                {t('information2')}
+              </div>
+              <div className={classNames("my-2 m indent-5 text-justify")}>
+                {t('information3')}
+              </div>
+              <div className={classNames("my-2 m indent-5 text-justify")}>
+                {t('information4')}
+              </div>
+
+
+            </div>
+            <Image
+              className="p-5 float-right"
+              src={roman}
+              width={330}
+              height={480}
+              alt={""}>
+            </Image>
+          </div>
+          <div className={classNames("my-2 m indent-5 text-justify", styles["aboutText"])}>
+            {t('information5')}
+          </div>
 
           <div
             className={classNames(
-              "flex flex-column justify-center items-start my-3",
-              styles["aboutText"],
+              "flex flex-1 flex-col items-start",
+              styles["aboutDecisions"],
             )}
           >
-            <div className={classNames("my-2 indent-5 text-justify")}>
-              {t('information1')}
+            <div className={styles.stroke}></div>
+            <div className="text-2xl text-blue-900 flex justify-center self-center mb-5">
+              {t('implemented')}
             </div>
-            <div className={classNames("my-2 indent-5 text-justify")}>
-              {t('information2')}
-            </div>
-            <div className={classNames("my-2 m indent-5 text-justify")}>
-              {t('information3')}
-            </div>
-            <div className={classNames("my-2 m indent-5 text-justify")}>
-              {t('information4')}
-            </div>
-
-
-          </div>
-          <Image
-            className="p-5 float-right"
-            src={roman}
-            width={330}
-            height={480}
-            alt={""}>
-          </Image>
-        </div>
-        <div className={classNames("my-2 m indent-5 text-justify", styles["aboutText"])}>
-          {t('information5')}
-        </div>
-
-        <div
-          className={classNames(
-            "flex flex-1 flex-col items-start",
-            styles["aboutDecisions"],
-          )}
-        >
-          <div className={styles.stroke}></div>
-          <div className="text-2xl text-blue-900 flex justify-center self-center mb-5">
-            {t('implemented')}
-          </div>
-          <div className="flex flex-row justify-between items-start self-start h-[650px] w-[850px]">
-            <div>
-              <SliderAbout locale={locale} />
-            </div>
-            <div>
-              <div className={classNames("px-5 indent-5 text-justify w-[548x]", styles["aboutText1"])}>
-                <p>{t('information1')}</p><br />
-                <p>{t('information1')}</p><br />
-                <p>{t('information1')}</p><br />
-                <p>{t('information1')}</p><br />
-                <p>{t('information1')}</p><br />
+            <div className="flex flex-row justify-between items-start self-start h-[650px] w-[850px]">
+              <div>
+                <SliderAbout locale={locale} />
+              </div>
+              <div>
+                <div className={classNames("px-5 indent-5 text-justify w-[548x]", styles["aboutText1"])}>
+                  <p>{t('information1')}</p><br />
+                  <p>{t('information1')}</p><br />
+                  <p>{t('information1')}</p><br />
+                  <p>{t('information1')}</p><br />
+                  <p>{t('information1')}</p><br />
+                </div>
               </div>
             </div>
-          </div>
 
 
-          {/* <div className="text-2xl flex flex-row justify-between mb-5 w-full px-5">
+            {/* <div className="text-2xl flex flex-row justify-between mb-5 w-full px-5">
             <Link href="/about/ohmatdet"><div
               className={classNames(
                 "flex flex-col justify-center items-center rounded-xl",
@@ -138,9 +139,10 @@ const AboutPage = ({ params: { locale } }: { params: { locale: string } }) => {
               /></div>
             </Link>
           </div> */}
+          </div>
         </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </Suspense>
   );
 };
 
