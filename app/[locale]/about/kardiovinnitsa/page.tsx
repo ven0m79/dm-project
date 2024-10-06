0797e9e0129a36d289392d58c980ba532e0d3cba
ven0m <yermolenko79@gmail.com>
2024-10-06 16:22:57 +0300

    correct-vercel4

--- app/[locale]/about/kardiovinnitsa/page.tsx
+++ app/[locale]/about/kardiovinnitsa/page.tsx
@@ -1,5 +1,6 @@
-"use client"
-import React from "react";
+"use client";
+
+import React, { Suspense } from "react";
 import classNames from "classnames";
 import { MainLayout } from "@app/[locale]/components/templates";
 import styles from "./Kardio.module.css";
@@ -7,13 +8,16 @@ import { useTranslations } from 'next-intl';
 import { Link } from "config";
 import Image from 'next/image';
 
-
 const KardioPage = ({ params: { locale } }: { params: { locale: string } }) => {
-
   const t = useTranslations('AboutPage');
+
   return (
     <MainLayout>
-      <div className=" text-black self-center h-96 text-5xl">Вінницький Кардіоцентр</div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="text-black self-center h-96 text-5xl">
+          Вінницький Кардіоцентр
+        </div>
+      </Suspense>
     </MainLayout>
   );
 };
