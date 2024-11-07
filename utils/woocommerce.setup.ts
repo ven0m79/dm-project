3b6835dab8e0a5c3d51db90ce39f47d9dfed9480
ven0m <yermolenko79@gmail.com>
2024-11-07 13:59:56 +0200

    realesed-from-vercel

--- utils/woocommerce.setup.ts
+++ utils/woocommerce.setup.ts
@@ -7,7 +7,7 @@ import {
 import { title } from "process";
 
 export const api = new WooCommerceRestApi({
-  url: "https://dm-project.com.ua",
+  url: "https://api.dm-project.com.ua",
   consumerKey: "ck_8dee30956004b4c7f467a46247004a2f4cd650e5",
   consumerSecret: "cs_1cf0a573275e5cafe5af6bddbb01f29b9592be20",
   version: "wc/v3",
