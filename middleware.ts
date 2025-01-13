482fcdace8caf1db5eed55067edb21075fed3593
ven0m <yermolenko79@gmail.com>
2025-01-13 10:43:19 +0200

    fix:default-locale

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,25 @@
 import createMiddleware from "next-intl/middleware";
 import { locales, localePrefix } from "./config";
+import { NextResponse } from "next/server";
 
-export default createMiddleware({
-  locales,
-  defaultLocale: "ua",
-  localePrefix,
-  localeDetection: true,
-});
+export function middleware(req: { nextUrl: { pathname: any; clone: () => any; }; cookies: { get: (arg0: string) => { (): any; new(): any; value: any; }; }; }) {
+  const pathname = req.nextUrl.pathname;
+  const locale = pathname.split('/')[1];
+  const supportedLocales = ['ua', 'en'];
+
+  // Локаль из куки
+  const cookieLocale = req.cookies.get('locale')?.value;
+
+  // Если локаль отсутствует в пути и не задана в куки, перенаправляем на defaultLocale
+  if (!supportedLocales.includes(locale)) {
+    const defaultLocale = cookieLocale && supportedLocales.includes(cookieLocale) ? cookieLocale : 'ua';
+    const url = req.nextUrl.clone();
+    url.pathname = `/${defaultLocale}${pathname}`;
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/((?!api|_next|.*\\..*).*)"],
