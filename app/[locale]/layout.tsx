f3d0a4d5ab2b80f6250ee37f7c58c0cda8d520e3
ven0m <yermolenko79@gmail.com>
2025-01-26 16:41:29 +0200

    fix:seo optimization to about page1

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,8 @@
 import { Roboto } from "next/font/google";
 import { ReactNode } from "react";
 
-import { DefaultSeo } from "next-seo";
-import defaultSEOConfig from "../../next-seo.config";
-import { usePathname } from "next/navigation";
+import { NextSeo } from "next-seo";
+
 
 
 import { NextIntlClientProvider, useMessages } from "next-intl";
@@ -37,21 +36,10 @@ export default function RootLayout({
 }: LocaleLayoutProps) {
   unstable_setRequestLocale(locale);
   const messages = useMessages();
-  const pathname = usePathname();
-  const seoConfig = {
-    ...defaultSEOConfig,
-    openGraph: {
-      ...defaultSEOConfig.openGraph,
-      locale: locale === "en" ? "en_US" : "uk_UA", // Установка языка.
-      url: `https://dm-project.com.ua${pathname}`, // Динамическое URL.
-    },
-  };
-
-
 
   return (
     <html lang={locale}>
-      <head>
+       <head>
       </head>
       <body
         className={cn(
@@ -61,7 +49,6 @@ export default function RootLayout({
       >
         <main className="flex flex-1 flex-col items-center justify-center gap-12 w-100% bg-white">
           <NextIntlClientProvider locale={locale} messages={messages}>
-          <DefaultSeo {...seoConfig} />
             {children}
           </NextIntlClientProvider>
         </main>
