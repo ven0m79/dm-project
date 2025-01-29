fbc1ae1a3428410b58345662023a570bd0158fae
ven0m <yermolenko79@gmail.com>
2025-01-29 11:48:34 +0200

    add mobile header anf desktop header with max

--- app/[locale]/components/molecules/header/Header.tsx
+++ app/[locale]/components/molecules/header/Header.tsx
@@ -6,18 +6,11 @@ import styles from "./Header.module.css";
 import Image from "next/image";
 import { Link, usePathname } from "../../../../../config";
 import { useTranslations, useLocale } from "next-intl";
-import clsx from "clsx";
-import Loader from "@app/[locale]/components/atoms/loader/loaderSearch";
-import {
-  Combobox,
-  ComboboxInput,
-  ComboboxOption,
-  ComboboxOptions,
-} from "@headlessui/react";
-import debounce from "lodash.debounce";
-import { useSearchParams } from "next/navigation";
-import { useIsMobile } from "@app/[locale]/components/hooks/useIsMobile";
 
+import debounce from "lodash.debounce";
+import { useIsMobile } from "@app/[locale]/components/hooks/useIsMobile";
+import MobileHeader from "./MobileHeader";
+import DesktopHeader from "./DesktopHeader";
 
 
 const api = axios.create({
@@ -42,136 +35,12 @@ type Product = {
 }
 
 
-
-const MobileHeader: FC<{searchTerm: string, loading: boolean, setSearchTerm: Dispatch<SetStateAction<string>>, products: Product[]}> = ({searchTerm, setSearchTerm, loading, products}) => {
-  const t = useTranslations("Header");
-  const t2 = useTranslations("Index");
-  
-  const highlightText = (text: string, highlight: string) => {
-    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
-    return (
-      <>
-        {parts.map((part, i) => (
-          <span
-            key={i}
-            style={{
-              color:
-                part.toLowerCase() === highlight.toLowerCase() ? "blue" : "",
-            }}
-          >
-            {part}
-          </span>
-        ))}
-      </>
-    );
-  };
-
-  return <div className="flex flex-1 flex-col self-start  h-svh border-2 border-cyan-500">
-    <div className={classNames("flex flex-1 flex-row justify-center w-screen", [styles.logo])}>
-      <div className="ml-4 w-1/4">
-        <Link
-          href={"/catalog"}
-          className="flex items-center">
-          <Image
-            src="/go-products.webp"
-            width={25}
-            height={25}
-            alt="Перейти до каталогу продуктів"
-          /> <span className="ml-2 font-bold">Продукти</span>
-        </Link>
-      </div>
-      <div className="flex justify-center w-1/2">
-        <Link href={"/home"}>
-          <Image
-            src="/logo-DM-project.png"
-            width={120}
-            height={75}
-            alt="Logo DM Project"
-          />
-        </Link>
-      </div>
-      <div className="flex justify-center w-1/4">
-        <Link href={"/catalog"}>
-          <Image
-            src="/go-menu.webp"
-            width={35}
-            height={35}
-            alt="Відкити меню"
-          />
-        </Link>
-      </div>
-    </div>
-    <div className={classNames("flex justify-center items-center h-[30px] text-white text-nowrap bg-[#0061AA]", styles["backText"])}>
-      {t2("authorized-representativeDreger1")}<span className="font-bold mx-1">{t2("authorized-representativeDreger2")}</span> {t2("authorized-representativeDreger3")}
-    </div>
-    <div className={classNames("flex justify-center items-center pb-2 h-[50px] bg-[#0061AA]", styles["searchMob"])}>
-      <Combobox value={searchTerm}>
-        <div className="relative flex z-50 w-[90%]">
-          <ComboboxInput
-            className={clsx(
-              "w-full rounded-lg border-[#0061AA] border bg-white py-1 pr-8 pl-3 text-sm/6 text-black",
-              "focus: outline-none data-[focus]:outline-none data-[focus]:-outline-offset-2 data-[focus]:bg-sky-50",
-            )}
-            placeholder={t("placeholder")}
-            onChange={(event) => setSearchTerm(event.target.value)}
-          />
-
-          {loading && (
-            <div className="absolute right-5 mt-4">
-              <Loader />
-            </div>
-          )}
-
-          {products.length > 0 && (
-            <ComboboxOptions
-              anchor="bottom"
-              transition
-              className={clsx(
-                "w-[var(--input-width)] rounded-xl border-2 border-[#0061AA] bg-white p-1 [--anchor-gap:var(--spacing-1)] empty:invisible z-50",
-                "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0",
-              )}
-            >
-              {products.map((product) => (
-                <ComboboxOption
-                  key={product.id}
-                  value={product.name}
-                  className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-sky-100"
-                >
-                  <a
-                    className={clsx("block, text-black")}
-                    href={`/catalog/sub-catalog/product/${product.id}?category=${product.tags[0].name}`}
-                  >
-                    <div className="text-sm/6 text-black">
-                      {highlightText(product.name, searchTerm)}
-                    </div>
-                  </a>
-                </ComboboxOption>
-              ))}
-            </ComboboxOptions>
-          )}
-        </div>
-      </Combobox>
-
-    </div>
-  </div>
-
-}
-
-
-
-
 const Header = () => {
-  const t = useTranslations("Header");
-  const t2 = useTranslations("Index");
   const locale = useLocale();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
   const [searchTerm, setSearchTerm] = useState("");
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(false);
-
   const isMobile = useIsMobile();
-  console.log({ isMobile });
 
   const handleSearch = async (term: string) => {
     if (term.length < 3) {
@@ -219,182 +88,13 @@ const Header = () => {
     };
   }, [searchTerm, debouncedHandleSearch]);
 
-  const highlightText = (text: string, highlight: string) => {
-    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
-    return (
-      <>
-        {parts.map((part, i) => (
-          <span
-            key={i}
-            style={{
-              color:
-                part.toLowerCase() === highlight.toLowerCase() ? "blue" : "",
-            }}
-          >
-            {part}
-          </span>
-        ))}
-      </>
-    );
-  };
-
-  const selectedCategory = useMemo(() => {
-    return searchParams?.get("category")
-      ? `?category=${searchParams?.get("category")}`
-      : "";
-  }, [searchParams]);
-
-
-
   return (
-    <header
-      className={classNames("mt-top flex flex-col", styles["header"])}
-    >
-      {typeof window !== "undefined" && isMobile ? <MobileHeader searchTerm={searchTerm} setSearchTerm={setSearchTerm} loading={loading} products={products} /> :
-        <>
-          <div className={classNames("w-full", styles["lang"])}>
-            <div className={styles.langText}>
-              <Link href={`${pathname}${selectedCategory}`} locale="en">
-                EN
-              </Link>
-              {"   "}
-              <Link href={`${pathname}${selectedCategory}`} locale="ua">
-                UA
-              </Link>
-            </div>
-          </div>
-          <div className={styles.containerWithLogo}>
-            <div className={styles.logo}>
-              <Link href={"/home"}>
-                <Image
-                  src="/logo-DM-project.png"
-                  width={120}
-                  height={75}
-                  alt="Logo DM Project"
-                />
-              </Link>
-            </div>
-            <div className={styles.contactsGroupIcons}>
-              <Image
-                src="/telephone.png"
-                width={40}
-                height={40}
-                alt="Logo DM Project"
-              />
-            </div>
-            <div
-              className={classNames(
-                "text-[8px] sm:text-[10px] xl:text-[16px]",
-                styles["contactsGroup"],
-              )}
-            >
-              <div>+380 44 520-12-24</div>
-              <div className="pt-1">+380 66 358-98-10</div>
-              <div className="pl-10">(cервіс)</div>
-            </div>
-            <div className={styles.contactsGroupIcons}>
-              <Image
-                src="/email.png"
-                width={40}
-                height={40}
-                alt="Logo DM Project"
-              />
-            </div>
-            <div
-              className={classNames(
-                "text-[8px] sm:text-[10px] xl:text-[16px]",
-                styles["contactsGroup"],
-              )}
-            >
-              <div>allinfo@dm-project.com.ua</div>
-              <div>sales@dm-project.com.ua</div>
-              <div>service@dm-project.com.ua</div>
-            </div>
-            <div className={classNames("mx-auto max-w-76", styles["search"])}>
-              <Combobox value={searchTerm}>
-                <div className="relative flex z-50">
-                  <ComboboxInput
-                    className={clsx(
-                      "w-full rounded-lg border-[#0061AA] border bg-white/5 py-1.5 pr-8 pl-3 text-sm/6 text-black",
-                      "focus: outline-none data-[focus]:outline-none data-[focus]:-outline-offset-2 data-[focus]:bg-sky-50",
-                    )}
-                    placeholder={t("placeholder")}
-                    onChange={(event) => setSearchTerm(event.target.value)}
-                  />
-
-                  {loading && (
-                    <div className="absolute right-5 mt-4">
-                      <Loader />
-                    </div>
-                  )}
-
-                  {products.length > 0 && (
-                    <ComboboxOptions
-                      anchor="bottom"
-                      transition
-                      className={clsx(
-                        "w-[var(--input-width)] rounded-xl border-2 border-[#0061AA] bg-white p-1 [--anchor-gap:var(--spacing-1)] empty:invisible z-50",
-                        "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0",
-                      )}
-                    >
-                      {products.map((product) => (
-                        <ComboboxOption
-                          key={product.id}
-                          value={product.name}
-                          className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-sky-100"
-                        >
-                          <a
-                            className={clsx("block, text-black")}
-                            href={`/catalog/sub-catalog/product/${product.id}?category=${product.tags[0].name}`}
-                          >
-                            <div className="text-sm/6 text-black">
-                              {highlightText(product.name, searchTerm)}
-                            </div>
-                          </a>
-                        </ComboboxOption>
-                      ))}
-                    </ComboboxOptions>
-                  )}
-                </div>
-              </Combobox>
-            </div>
-            <div className="flex flex-row pl-10">
-              <div
-                className={classNames(
-                  "flex flex-row justify-center items-center",
-                  styles["socialMedia"],
-                )}
-              >
-                <Link href="http://youtube.com">
-                  <Image
-                    src="/youtube-ico.jpg"
-                    width={30}
-                    height={30}
-                    alt="Logo Youtube"
-                  />
-                </Link>
-              </div>
-              <div
-                className={classNames(
-                  "flex flex-column justify-center items-center",
-                  styles["socialMedia"],
-                )}
-              >
-                <Link href="http://facebook.com">
-                  <Image
-                    src="/facebook-ico.jpg"
-                    width={30}
-                    height={30}
-                    alt="Logo Facebook"
-                  />
-                </Link>
-              </div>
-            </div>
-          </div>
-        </>}
+    <header className={classNames("mt-top flex flex-col", styles["header"])}>
+      {typeof window !== "undefined" && isMobile ? 
+      <MobileHeader searchTerm={searchTerm} setSearchTerm={setSearchTerm} loading={loading} products={products} /> 
+      :
+      <DesktopHeader searchTerm={searchTerm} setSearchTerm={setSearchTerm} loading={loading} products={products} /> }
     </header >
-
-
   );
 };
 
---
+++ app/[locale]/components/molecules/header/MobileHeader.tsx
@@ -0,0 +1,150 @@
+"use client";
+import classNames from "classnames";
+import React, { Dispatch, FC, SetStateAction, useEffect, useMemo, useState } from "react";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import { Link, usePathname } from "../../../../../config";
+import { useTranslations, useLocale } from "next-intl";
+import clsx from "clsx";
+import Loader from "@app/[locale]/components/atoms/loader/loaderSearch";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
+import debounce from "lodash.debounce";
+import { useSearchParams } from "next/navigation";
+import { useIsMobile } from "@app/[locale]/components/hooks/useIsMobile";
+
+type TagType = {
+    id: number;
+    name: string;
+    slug: string;
+  };
+
+type Product = {
+    tags: TagType[];
+    id: number;
+    name: string;
+    price: string;
+    permalink: string;
+  }
+  
+  const MobileHeader: FC<{searchTerm: string, loading: boolean, setSearchTerm: Dispatch<SetStateAction<string>>, products: Product[]}> = ({searchTerm, setSearchTerm, loading, products}) => {
+    const t = useTranslations("Header");
+    const t2 = useTranslations("Index");
+    
+    const highlightText = (text: string, highlight: string) => {
+    const parts = text.split(new RegExp(`(${highlight})`, "gi"));
+      return (
+        <>
+          {parts.map((part, i) => (
+            <span
+              key={i}
+              style={{
+                color:
+                  part.toLowerCase() === highlight.toLowerCase() ? "blue" : "",
+              }}
+            >
+              {part}
+            </span>
+          ))}
+        </>
+      );
+    };
+  
+
+    return <div className="flex flex-1 flex-col self-start  h-svh border-2 border-cyan-500">
+      <div className={classNames("flex flex-1 flex-row justify-center w-screen", [styles.logo])}>
+        <div className="ml-4 w-1/4">
+          <Link
+            href={"/catalog"}
+            className="flex items-center">
+            <Image
+              src="/go-products.webp"
+              width={25}
+              height={25}
+              alt="Перейти до каталогу продуктів"
+            /> <span className="ml-2 font-bold">Продукти</span>
+          </Link>
+        </div>
+        <div className="flex justify-center w-1/2">
+          <Link href={"/home"}>
+            <Image
+              src="/logo-DM-project.png"
+              width={120}
+              height={75}
+              alt="Logo DM Project"
+            />
+          </Link>
+        </div>
+        <div className="flex justify-center w-1/4">
+          <Link href={"/catalog"}>
+            <Image
+              src="/go-menu.webp"
+              width={35}
+              height={35}
+              alt="Відкити меню"
+            />
+          </Link>
+        </div>
+      </div>
+      <div className={classNames("flex justify-center items-center h-[30px] text-white text-nowrap bg-[#0061AA]", styles["backText"])}>
+        {t2("authorized-representativeDreger1")}<span className="font-bold mx-1">{t2("authorized-representativeDreger2")}</span> {t2("authorized-representativeDreger3")}
+      </div>
+      <div className={classNames("flex justify-center items-center pb-2 h-[50px] bg-[#0061AA]", styles["searchMob"])}>
+        <Combobox value={searchTerm}>
+          <div className="relative flex z-50 w-[90%]">
+            <ComboboxInput
+              className={clsx(
+                "w-full rounded-lg border-[#0061AA] border bg-white py-1 pr-8 pl-3 text-sm/6 text-black",
+                "focus: outline-none data-[focus]:outline-none data-[focus]:-outline-offset-2 data-[focus]:bg-sky-50",
+              )}
+              placeholder={t("placeholder")}
+              onChange={(event) => setSearchTerm(event.target.value)}
+            />
+  
+            {loading && (
+              <div className="absolute right-5 mt-4">
+                <Loader />
+              </div>
+            )}
+  
+            {products.length > 0 && (
+              <ComboboxOptions
+                anchor="bottom"
+                transition
+                className={clsx(
+                  "w-[var(--input-width)] rounded-xl border-2 border-[#0061AA] bg-white p-1 [--anchor-gap:var(--spacing-1)] empty:invisible z-50",
+                  "transition duration-100 ease-in data-[leave]:data-[closed]:opacity-0",
+                )}
+              >
+                {products.map((product) => (
+                  <ComboboxOption
+                    key={product.id}
+                    value={product.name}
+                    className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-sky-100"
+                  >
+                    <a
+                      className={clsx("block, text-black")}
+                      href={`/catalog/sub-catalog/product/${product.id}?category=${product.tags[0].name}`}
+                    >
+                      <div className="text-sm/6 text-black">
+                        {highlightText(product.name, searchTerm)}
+                      </div>
+                    </a>
+                  </ComboboxOption>
+                ))}
+              </ComboboxOptions>
+            )}
+          </div>
+        </Combobox>
+  
+      </div>
+    </div>
+  
+  }
+
+  
+export default MobileHeader;
