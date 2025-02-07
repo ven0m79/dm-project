43988cdab7afbc44ed912ef9ed0d3fd5baf8f73e
ven0m <yermolenko79@gmail.com>
2025-02-07 17:49:40 +0200

    fix:metadata dynamic

---
+++ app/[locale]/catalog/client-page.tsx
@@ -0,0 +1,198 @@
+"use client";
+import React from "react";
+import styles from "./Catalog.module.css";
+
+import { MainLayout } from "@app/[locale]/components/templates";
+
+import { Link } from "config";
+import Image from "next/image";
+import classNames from "classnames";
+import imgCons from "./icons-photo/consumables.webp";
+import imgFurniture from "./icons-photo/furniture-med.webp";
+import imgIntensive from "./icons-photo/intencive-terapy.webp";
+import imgMedgaz from "./icons-photo/med-gaz.webp";
+import imgNeonat from "./icons-photo/neonat.webp";
+import imgOper from "./icons-photo/oper.webp";
+import imgSteriliz from "./icons-photo/sterilization.webp";
+import { useTranslations } from "next-intl";
+import { useIsMobile } from "@app/[locale]/components/hooks/useIsMobile"; 
+
+export const ClientPage = ({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) => {
+  const t = useTranslations("Catalog1");
+
+  const isMobile = useIsMobile();
+  console.log({ isMobile });
+
+  return (
+    <MainLayout>
+      <div
+        className={classNames(
+          "text-2xl flex flex-wrap justify-start mb-5 gap-5 mt-5",
+          [styles.catalogContainer],
+        )}
+      >
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=or-equipment",
+          }}
+        >
+          {/* sm:bg-amber-800 in classnames */}
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("or-equipment")}</span>
+            <Image
+              className={styles.img}
+              src={imgOper}
+              width={130}
+              height={130}
+              alt={t("or-equipment")}
+            />
+          </div>
+        </Link>
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=icu-equipment",
+          }}
+        >
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("icu-equipment")}</span>
+            <Image
+              className={styles.img}
+              src={imgIntensive}
+              width={130}
+              height={130}
+              alt={t("icu-equipment")}
+            />
+          </div>
+        </Link>
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=neonatal-equipment",
+          }}
+        >
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("neonatal-equipment")}</span>
+            <Image
+              className={styles.img}
+              src={imgNeonat}
+              width={130}
+              height={130}
+              alt={t("neonatal-equipment")}
+            />
+          </div>
+        </Link>
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=cleaning-and-desinfecting-equipment",
+          }}
+        >
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("candd-equipment")}</span>
+            <Image
+              className={styles.img}
+              src={imgSteriliz}
+              width={130}
+              height={130}
+              alt={t("candd-equipment")}
+            />
+          </div>
+        </Link>
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=gas-management-systems",
+          }}
+        >
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("gas-systems")}</span>
+            <Image
+              className={styles.img}
+              src={imgMedgaz}
+              width={130}
+              height={130}
+              alt={t("gas-systems")}
+            />
+          </div>
+        </Link>
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=furniture",
+          }}
+        >
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("other-equipment")}</span>
+            <Image
+              className={styles.img}
+              src={imgFurniture}
+              width={130}
+              height={130}
+              alt={t("other-equipment")}
+            />
+          </div>
+        </Link>
+        <Link
+          href={{
+            pathname: "/catalog/sub-catalog",
+            query: "category=accessories",
+          }}
+        >
+          <div
+            className={classNames(
+              "flex flex-row items-center rounded-xl mx-1",
+              styles["block-decisions"],
+            )}
+          >
+            <span className={styles.span}>{t("accessories")}</span>
+            <Image
+              className={styles.img}
+              src={imgCons}
+              width={130}
+              height={130}
+              alt={t("accessories")}
+            />
+          </div>
+        </Link>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default ClientPage;
--- app/[locale]/catalog/sub-catalog/page.tsx
+++ app/[locale]/catalog/sub-catalog/page.tsx
@@ -1,182 +1,19 @@
-"use client";
-import classNames from "classnames";
-import Link from "next/link";
-import React, { useCallback, useEffect, useMemo } from "react";
+import type { Metadata } from "next";
 
-import { fetchWooCommerceCategories } from "../../../../utils/woocommerce.setup";
-import styles from "./Sub-catalog.module.css";
-import { categoriesCreation, TransformedCategoriesType } from "./helpers";
-import Seo from "@app/[locale]/components/atoms/seo/Seo";
-import { useSidebar } from "@app/[locale]/components/contexts/products-sidebar/products-sidebar.context";
-import { getCategoriesIds } from "@app/[locale]/components/constants";
+import { ClientPage } from "./client-page";
 
-const SubCatalog = ({ params: { locale } }: { params: { locale: string } }) => {
-  const {
-    selectedProducts,
-    setCategories,
-    selectedCategory,
-    setSelectedCategoryId,
-    getCategoryDetails,
-    setOpenedCategoryIds,
-    openedCategoryIds,
-  } = useSidebar();
-
-  const currentIdsData = useMemo(() => getCategoriesIds(locale), [locale]);
-
-  const getData = useCallback(async () => {
-    try {
-      const data = await fetchWooCommerceCategories(locale);
-
-      if (data) {
-        setCategories(
-          categoriesCreation(data as unknown as TransformedCategoriesType[]),
-        );
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }): Promise<Metadata> {
+  return locale === "ua"
+    ? {
+        title: `ДМ-ПРОЕКТ: ${selectedCategory}`,
+        description: "Пропонуємо Вам сучасне обладнання різних типів та призначень.",
       }
-    } catch (e) {
-      console.warn({ e });
-    }
-  }, [locale, setCategories]);
+    : {
+        title: `DM-PROJECT: ${selectedCategory}`,
+        description: "Today, the company's main activities include not only the sale of high-quality medical equipment, but also the development and implementation of integrated solutions for medical institutions, from design to service and staff training.",
+      };
+}
 
-  useEffect(() => {
-    getData();
-  }, [getData, locale]);
-
-  const isAccessories = selectedProducts?.map((el) =>
-    el.tags.map((el) => el.name).includes("accessories"),
-  );
-
-  useEffect(() => {
-   
-    if (selectedCategory && openedCategoryIds.length === 0) {
-      console.log(selectedCategory)
-      // @ts-ignore
-      const categoryId = currentIdsData?.[selectedCategory];
-
-      if (categoryId) {
-        getCategoryDetails(categoryId, locale);
-        setSelectedCategoryId(categoryId);
-        setOpenedCategoryIds([categoryId]);
-      }
-    }
-  }, [
-    currentIdsData,
-    getCategoryDetails,
-    locale,
-    openedCategoryIds.length,
-    selectedCategory,
-    setOpenedCategoryIds,
-    setSelectedCategoryId,
-  ]);
-
-  return (
-    <>
-      <Seo
-        title={`ДМ-ПРОЕКТ: ${selectedCategory}`}
-        description={`Пропонуємо Вам сучасне обладнання різних типів та призначень`}
-      />
-      <div
-        className={classNames(
-          "flex flex-1 flex-row justify-between self-center mb-5",
-          styles.subCatalog,
-        )}
-      >
-        <div className="flex flex-wrap justify-start self-start mt-4 mb-4 mx-5 w-full items-start">
-          {selectedProducts && selectedProducts.length ? (
-            selectedProducts.map((el) => {
-              return isAccessories[0] ? (
-                <div
-                  key={el.id}
-                  className={classNames(
-                    "mx-1 mb-5 flex flex-col justify-center items-center",
-                    styles.headSubCatalogBlockMini,
-                  )}
-                >
-                  <div className="">
-                    <Link
-                      locale={locale}
-                      key={el.id}
-                      href={{
-                        pathname: `/catalog/sub-catalog/product/${el.translations[locale as any]}`,
-                        query: `category=${selectedCategory}`,
-                      }}
-                    >
-                      <div>{el.sku}</div>
-                      <div
-                        className={"cursor-pointer flex flex-1 justify-center"}
-                      >
-                        <img
-                          src={el.images[0].src}
-                          alt={el.images[0].alt}
-                          width={130}
-                          height={137}
-                        />
-                        {/* <div className="h-px mt-24 ml-20 text-[16px] flex self-center absolute text-red-500 font-bold">{el.price} $</div> */}
-                      </div>
-                      <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
-                      <div className="flex justify-center h-16">
-                        <h3 className="">{el.name} </h3>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              ) : (
-                <div
-                  key={el.id}
-                  className={classNames(
-                    "mx-5 mb-5 flex flex-col justify-center items-center",
-                    styles.headSubCatalogBlock,
-                  )}
-                >
-                  <div className="">
-                    <Link
-                      locale={locale}
-                      key={el.id}
-                      href={{
-                        pathname: `/catalog/sub-catalog/product/${el.translations[locale as any]}`,
-                        query: `category=${selectedCategory}`,
-                      }}
-                    >
-                      <div className="cursor-pointer flex justify-center">
-                        <img
-                          src={el.images[0].src}
-                          alt={el.images[0].alt}
-                          width={200}
-                          height={250}
-                        />
-                      </div>
-
-                      <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
-                      <div className="flex justify-center">
-                        <h3 className="flex justify-center h-16">{el.name}</h3>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <h2 className="text-amber-700"></h2>
-          )}
-        </div>
-        {/* 
-          <div className={classNames("mt-4", styles.subMenu)}>
-            {locale === "ua" ? (
-              <RSidebar
-                items={[categories?.[0] || []]}
-                setSelectedProducts={setSelectedProducts}
-                locale={locale}
-              />
-            ) : (
-              <RSidebar
-                items={[categories?.[1] || []]}
-                setSelectedProducts={setSelectedProducts}
-                locale={locale}
-              />
-            )}
-          </div> */}
-      </div>
-    </>
-  );
-};
-
-export default SubCatalog;
+export default function Page({ params: { locale } }: { params: { locale: string } }) {
+  return <ClientPage locale={locale} selectedCategory={""} />;
+}
