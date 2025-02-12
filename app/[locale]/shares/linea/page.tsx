4716429d77877da4d72f0e7ddc8e92c635a9468f
ven0m <yermolenko79@gmail.com>
2025-02-12 12:10:24 +0200

    fix:shares-polaris200

--- app/[locale]/shares/linea/page.tsx
+++ app/[locale]/shares/linea/page.tsx
@@ -1,79 +1,19 @@
-"use client"
-import React from "react";
-import styles from "./../Shares.module.css";
-import { MainLayout } from "@app/[locale]/components/templates";
-import Image from "next/image";
-import classNames from "classnames";
-import linea1 from "./linea1.webp";
-import linea2 from "./linea2.webp";
-import linea3 from "./linea3.webp";
-import { useTranslations } from "next-intl";
+import type { Metadata } from "next";
 
+import  SharesLinea from "./client-page";
 
-const SharesLinea = () => {
-    const t = useTranslations('Shares');
-    return (
-        <MainLayout>
-            <div
-                className={classNames(
-                    "w-full flex flex-wrap justify-center mb-3 mt-3",
-                )}
-            >
-                <div className={classNames(
-                    "flex flex-col justify-center mb-5 w-[300px]",
-                    [styles.catalogContainerDeeper])}>
-                    <h3 className="text-justify text-[18px] font-bold sm:text-[20px]">{t('shares-linea-title')}</h3>
-                    <div className="flex flex-wrap">
-                        <p className="indent-5 text-justify">{t('shares-linea-prehead')} <span className="font-bold">2 996,00 у.о.</span></p>
-                        <div className="w-full flex flax-wrap justify-center my-2">
-                            <Image
-                                className={classNames("w-1/2",styles.img)}
-                                src={linea1}
-                                width={400}
-                                height={100}
-                                alt="linea"
-                            />
-                            <Image
-                                className={classNames("w-1/2",styles.img)}
-                                src={linea2}
-                                width={400}
-                                height={100}
-                                alt="linea"
-                            />
-                        </div>
-                        <div className="w-screen">
-                            -	{t('shares-linea-specific1')}<br />
-                            -	{t('shares-linea-specific2')}<br />
-                            -	{t('shares-linea-specific3')}<br />
-                            -	{t('shares-linea-specific4')}<br />
-                            -	{t('shares-linea-specific5')}<br />
-                            -	{t('shares-linea-specific6')}<br />
-                            -	{t('shares-linea-specific7')}<br />
-                            -	{t('shares-linea-specific8')}<br />
-                            -	{t('shares-linea-specific9')}<br />
-                            -	{t('shares-linea-specific10')}<br />
-                        </div>
+export async function generateMetadata({ params: { locale } }: { params: { locale: string } }): Promise<Metadata> {
+  return locale === "ua"
+    ? {
+        title: "Консоль медична з настінним кріпленням Лінеа IM2",
+        description: "Лінійка продуктів Linea розроблена для використання практично у всіх сферах. У звичайних палатах, відділеннях інтенсивної терапії, наркозних/операційних, реабілітації – завдяки своїй модульній конструкції сімейство Linea є індивідуальним рішенням для подачі медіа для багатьох застосувань у лікарні.",
+      }
+    : {
+        title: "Wall medical supply unit Linea",
+        description: "The Linea product range is designed for use in almost all areas. In conventional wards, intensive care units, anesthesia/operating rooms, rehabilitation – thanks to its modular design, the Linea family is a customized media supply solution for many applications in the hospital.",
+      };
+}
 
-                        <div className={classNames("self-center my-2 indent-5 text-justify", styles["catalogContainer"])}>
-                            {/* <p className="my-2">Поєднайте ергономіку та економічність з використанням настінних консолей (панелей), які нададуть вам електричні, газові та ІТ-рішення.</p> */}
-                            <Image
-                                className={"w-1/2 float-left mt-1 mb-5 mr-5"}
-                                src={linea3}
-                                width={500}
-                                height={100}
-                                alt="linea"
-                            />
-                            <p className="my-2">{t('shares-linea-text1')}</p>
-                            <p className="my-2">{t('shares-linea-text2')}</p>
-                            {/* <p className="my-2">Як і всі рішення Dräger, сімейство продуктів Linea також допомагає зробити повсякденні робочі процеси ефективнішими. Dräger робить гнучкість і сумісність основними акцентами своєї продукції. Linea дає змогу ефективно використовувати особливості приміщення. Інтеграція стандартних рейок дозволяють використовувати ряд аксесуарів від Dräger та інших виробників, тому ви можете комбінувати аксесуари повністю відповідно до ваших вимог.</p> */}
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </MainLayout>
-    );
-};
-
-export default SharesLinea;
+export default function Page() {
+  return <SharesLinea  />;
+}
