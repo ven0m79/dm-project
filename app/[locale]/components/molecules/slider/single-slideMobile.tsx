1919ad6aa5d0c3a9ce5353e6bb903e629d066326
ven0m <yermolenko79@gmail.com>
2025-02-20 15:32:47 +0200

    fix:homepage-slider6

--- app/[locale]/components/molecules/slider/single-slideMobile.tsx
+++ app/[locale]/components/molecules/slider/single-slideMobile.tsx
@@ -17,7 +17,7 @@ const animatedStyles = {
 
 const animatedStylesForButton = {
   initial: {
-    x: "55%",
+    x: "35%",
     opacity: 0,
   },
   animate: {
@@ -97,7 +97,7 @@ const SingleSlideMobile: FC<ArticleSingleType> = ({
           transition={{ duration: 2, ease: "easeOut" }}
           style={{ fontSize: 20 }}
         >
-          <div className="mt-5 ml-3 w-1/2 text-sm leading-5">{description}</div>
+          <div className="mt-4 ml-3 w-1/2 text-sm leading-5">{description}</div>
         </motion.div>
       </div>
         <motion.a
