72171fb93ca90f2ebaccee0215d296a63a75c759
ven0m <yermolenko79@gmail.com>
2025-08-17 16:12:55 +0300

    fix: delete recursion in leftsidebar9-modifyUseEffect

--- app/[locale]/catalog/sub-catalog/client-page.tsx
+++ app/[locale]/catalog/sub-catalog/client-page.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import classNames from "classnames";
 import Link from "next/link";
 import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
-import { useSearchParams } from "next/navigation"; // 🆕 Імпортуємо useSearchParams
 
 import { fetchWooCommerceCategories } from "../../../../utils/woocommerce.setup";
 import styles from "./Sub-catalog.module.css";
@@ -12,6 +10,7 @@ import { categoriesCreation, TransformedCategoriesType } from "./helpers";
 import { useSidebar } from "@app/[locale]/components/contexts/products-sidebar/products-sidebar.context";
 import { getCategoriesIds } from "@app/[locale]/components/constants";
 import { useIsMobile } from "@app/[locale]/components/hooks/useIsMobile";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
   const {
@@ -21,13 +20,16 @@ export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
     setSelectedCategoryId,
     getCategoryDetails,
     setOpenedCategoryIds,
+    openedCategoryIds,
   } = useSidebar();
-
   const searchParams = useSearchParams();
-  const categorySlug = searchParams ? searchParams.get("category") : null; // 🆕 Отримуємо параметр з URL напряму
+  const categorySlug = searchParams ? searchParams.get("category") : null;
 
   const currentIdsData = useMemo(() => getCategoriesIds(locale), [locale]);
   const isMobile = useIsMobile();
+  const router = useRouter();
+  const isIOS = typeof window !== "undefined" && /iPhone|iPad|iPod/i.test(navigator.userAgent);
+
   const sortedProducts = [...selectedProducts].sort((a, b) => a.name.localeCompare(b.name));
   const [visibleCount, setVisibleCount] = useState(15);
   const productsToRender = sortedProducts.slice(0, visibleCount);
@@ -35,13 +37,20 @@ export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
   const getData = useCallback(async () => {
     try {
       const data = await fetchWooCommerceCategories(locale);
+
       if (data) {
-        setCategories(categoriesCreation(data as unknown as TransformedCategoriesType[]));
+        setCategories(
+          categoriesCreation(data as unknown as TransformedCategoriesType[]),
+        );
       }
     } catch (e) {
       console.warn({ e });
     }
   }, [locale, setCategories]);
+
+  const isAccessories = selectedProducts?.map((el) =>
+    el.tags.map((el) => el.name).includes("accessories"),
+  );
 
   // ✅ Викликаємо getData лише один раз при завантаженні компонента
   useEffect(() => {
@@ -59,11 +68,11 @@ export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
       }
     }
   }, [categorySlug, currentIdsData, locale, getCategoryDetails, setSelectedCategoryId, setOpenedCategoryIds]);
-  const isAccessories = selectedProducts?.some((el) => el.tags.some((tag) => tag.name === "accessories"));
+
 
   return (
     <>
-      {typeof window !== "undefined" && isMobile ? (
+      {typeof window !== "undefined" && isMobile ?
         <div
           className={classNames(
             "flex flex-1 flex-row justify-between self-center mb-5",
@@ -71,47 +80,94 @@ export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
           )}
         >
           <div className="flex flex-wrap justify-start self-start mt-4 mb-4 mx-1 w-full items-start">
+
             {productsToRender && productsToRender.length ? (
-              productsToRender.map((el) => (
-                <div
-                  key={el.id}
-                  className={classNames(
-                    "mx-1 mb-5 flex flex-col justify-center items-center",
-                    isAccessories ? styles.headSubCatalogBlockMini : styles.headSubCatalogBlock,
-                  )}
-                >
-                  <Link
-                    locale={locale}
-                    href={{
-                      pathname: `/catalog/sub-catalog/product/${el.translations[locale as any]}`,
-                      query: { category: selectedCategory },
-                    }}
+              productsToRender.map((el) => {
+                return isAccessories[0] ? (
+                  <div
+                    key={el.id}
+                    className={classNames(
+                      "mx-1 mb-5 flex flex-col justify-center items-center",
+                      styles.headSubCatalogBlockMini,
+                    )}
                   >
                     <div className="w-full">
-                      {isAccessories && <div>{el.sku.length > 7 ? `${el.sku.slice(0, 7)}..` : el.sku}</div>}
-                      <div className="cursor-pointer flex flex-1 justify-center">
-                        <img
-                          src={el.images[0].src}
-                          alt={el.images[0].alt}
-                          width={130}
-                          height={137}
-                          className="w-full h-auto"
-                        />
+                      <div
+                        key={el.id}
+                        className="cursor-pointer"
+                        onClick={() => {
+                          const url = `/catalog/sub-catalog/product/${el.translations[locale as any]}?category=${encodeURIComponent(selectedCategory || "")}`;
+                          if (isIOS) {
+                            router.push(url); // Программная навигация
+                          } else {
+                            window.location.href = url; // Прямая загрузка страницы
+                          }
+                        }}
+                      >
+                        <div>{el.sku.length > 7 ? `${el.sku.slice(0, 7)}..` : el.sku}</div>
+                        <div className="cursor-pointer flex flex-1 justify-center">
+                          <img
+                            src={el.images[0].src}
+                            alt={el.images[0].alt}
+                            width={130}
+                            height={137}
+                            className="w-full h-auto"
+                          />
+                        </div>
+                        <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
+                        <div className="flex justify-center h-16">
+                          <h3 className={classNames(styles.headSubCatalogTitle)}>{el.name}</h3>
+                        </div>
                       </div>
-                      <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
-                      <div className="flex justify-center h-16">
-                        <h3 className={classNames(styles.headSubCatalogTitle)}>{el.name}</h3>
+
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    key={el.id}
+                    className={classNames(
+                      "mx-1 mb-5 flex flex-col justify-center items-center",
+                      styles.headSubCatalogBlock,
+                    )}
+                  >
+                    <div className="w-full">
+                      <div
+                        key={el.id}
+                        className="cursor-pointer"
+                        onClick={() => {
+                          const url = `/catalog/sub-catalog/product/${el.translations[locale as any]}?category=${encodeURIComponent(selectedCategory || "")}`;
+                          if (isIOS) {
+                            router.push(url); // Программная навигация
+                          } else {
+                            window.location.href = url; // Прямая загрузка страницы
+                          }
+                        }}
+                      >
+                        <div className="cursor-pointer flex justify-center">
+                          <img
+                            src={el.images[0].src}
+                            alt={el.images[0].alt}
+                            width={isMobile ? 150 : 200}
+                            height={isMobile ? 150 : 200}
+                            className="w-full h-auto"
+                          />
+                        </div>
+
+                        <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
+                        <div className="flex justify-center">
+                          <h3 className="flex justify-center h-16">{el.name}</h3>
+                        </div>
                       </div>
                     </div>
-                  </Link>
-                </div>
-              ))
+                  </div>
+                );
+              })
             ) : (
               <h2 className="text-amber-700"></h2>
             )}
           </div>
         </div>
-      ) : (
+        :
         <div
           className={classNames(
             "flex flex-1 flex-row justify-between self-center mb-5",
@@ -120,51 +176,92 @@ export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
         >
           <div className="flex flex-wrap justify-start self-start mt-4 mb-4 mx-5 w-full max-w-[800px] items-start">
             {productsToRender && productsToRender.length ? (
-              productsToRender.map((el) => (
-                <div
-                  key={el.id}
-                  className={classNames(
-                    "mx-5 mb-5 flex flex-col justify-center items-center",
-                    isAccessories ? styles.headSubCatalogBlockMini : styles.headSubCatalogBlock,
-                  )}
-                >
-                  <Link
-                    locale={locale}
-                    href={{
-                      pathname: `/catalog/sub-catalog/product/${el.translations[locale as any]}`,
-                      query: { category: selectedCategory },
-                    }}
+              productsToRender.map((el) => {
+                return isAccessories[0] ? (
+                  <div
+                    key={el.id}
+                    className={classNames(
+                      "mx-1 mb-5 flex flex-col justify-center items-center",
+                      styles.headSubCatalogBlockMini,
+                    )}
                   >
                     <div className="w-full text-center">
-                      <div className="cursor-pointer flex justify-center">
-                        <img
-                          src={el.images[0].src}
-                          alt={el.images[0].alt}
-                          width={200}
-                          height={250}
-                          className="w-full h-auto"
-                        />
-                      </div>
-                      <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
-                      <div className="flex justify-center">
-                        <h3 className="flex justify-center h-16 w-full px-2">{el.name}</h3>
-                      </div>
+                      <Link
+                        locale={locale}
+                        key={el.id}
+                        href={{
+                          pathname: `/catalog/sub-catalog/product/${el.translations[locale as any]}`,
+                          query: `category=${selectedCategory}`,
+                        }}
+                      >
+                        <div className="w-full px-2">{el.sku.length > 7 ? `${el.sku.slice(0, 7)}..` : el.sku}</div>
+                        <div
+                          className={"cursor-pointer flex flex-1 justify-center w-full"}
+                        >
+                          <img
+                            src={el.images[0].src}
+                            alt={el.images[0].alt}
+                            width={130}
+                            height={137}
+                            className="w-full h-auto"
+                          />
+                          {/* <div className="h-px mt-24 ml-20 text-[16px] flex self-center absolute text-red-500 font-bold">{el.price} $</div> */}
+                        </div>
+                        <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
+                        <div className="flex justify-center h-16">
+                          <h3 className={classNames("text-center", styles.headSubCatalogTitle)}>{el.name} </h3>
+                        </div>
+                      </Link>
                     </div>
-                  </Link>
-                </div>
-              ))
+                  </div>
+                ) : (
+                  <div
+                    key={el.id}
+                    className={classNames(
+                      "mx-5 mb-5 flex flex-col justify-center items-center",
+                      styles.headSubCatalogBlock,
+                    )}
+                  >
+                    <div className="w-full text-center">
+                      <Link
+                        locale={locale}
+                        key={el.id}
+                        href={{
+                          pathname: `/catalog/sub-catalog/product/${el.translations[locale as any]}`,
+                          query: `category=${selectedCategory}`,
+                        }}
+                      >
+                        <div className="cursor-pointer flex justify-center">
+                          <img
+                            src={el.images[0].src}
+                            alt={el.images[0].alt}
+                            width={200}
+                            height={250}
+                            className="w-full h-auto"
+                          />
+                        </div>
+
+                        <div className="h-px bg-emerald-900 mb-1 mx-1 flex self-center"></div>
+                        <div className="flex justify-center">
+                          <h3 className="flex justify-center h-16 w-full px-2">{el.name}</h3>
+                        </div>
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })
             ) : (
               <h2 className="text-amber-700"></h2>
             )}
           </div>
+
         </div>
-      )}
+      }
       {sortedProducts.length > visibleCount && (
         <div className="flex flex-1 w-full self-center items-center justify-center">
           <button
             className={classNames("justify-center", styles.downloadable)}
-            onClick={() => setVisibleCount(visibleCount + 15)}
-          >
+            onClick={() => setVisibleCount(visibleCount + 15)}>
             Завантажити ще
           </button>
         </div>
