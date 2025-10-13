f3690c7b6c34a681d80a9c03caecf0b31e45945a
ven0m <yermolenko79@gmail.com>
2025-10-13 13:57:37 +0300

    fix: 2 in row prod-mx-0

--- app/[locale]/catalog/sub-catalog/client-page.tsx
+++ app/[locale]/catalog/sub-catalog/client-page.tsx
@@ -75,12 +75,12 @@ export const ClientPage: FC<{ locale: string }> = ({ locale }) => {
       </div>
 
       {/* Products */}
-      <div className="flex flex-wrap justify-start self-start mt-4 mb-4 mx-1 w-full items-start">
+      <div className="flex flex-wrap justify-start self-start mt-4 mb-4 mx-0 w-full items-start">
         {productsToRender?.length ? (
           productsToRender.map((el) => {
             const isAccessories = el.tags?.some((t) => t.name === "accessories");
             const cardClass = isAccessories
-              ? `mx-1 ${styles.headSubCatalogBlockMini}`
+              ? `mx-0 ${styles.headSubCatalogBlockMini}`
               : `mx-2 ${styles.headSubCatalogBlock}`;
 
             const url = `/catalog/sub-catalog/product/${el.translations[locale as any]}?category=${encodeURIComponent(selectedCategory || "")}`;
