c85bc97abeb4d1d2dd23e156393261af5fd06f39
ven0m <yermolenko79@gmail.com>
2025-11-22 17:10:51 +0200

    fix: 1st try optimize fetch categories5

--- app/[locale]/catalog/sub-catalog/api/categories.ts
+++
@@ -1,8 +0,0 @@
-import { fetchWooCommerceCategories } from "../../../../../utils/woocommerce.setup";
-import { categoriesCreation, TransformedCategoriesType } from "@app/[locale]/catalog/sub-catalog/helpers";
-
-// Серверна функція для отримання категорій з кешуванням
-export async function getCategories(locale: string): Promise<TransformedCategoriesType[]> {
-  const data = await fetchWooCommerceCategories(locale);
-  return categoriesCreation(data as TransformedCategoriesType[]);
-}
