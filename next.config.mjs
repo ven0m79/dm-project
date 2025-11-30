705c64e587aa97a9f0d9a2e4ec7ad8c200ab44a3
ven0m <yermolenko79@gmail.com>
2025-11-30 13:57:45 +0200

    fix: next.config - remove header for cache. -cache string

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,6 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 480, 768, 1024, 1280, 1600, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60, // 1 хвилина кешування, можна більше
   },
 
   // Хедери для кешування
