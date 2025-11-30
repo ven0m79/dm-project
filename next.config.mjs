82d7d34f7cc89238a394b6a5c802a130154c5784
ven0m <yermolenko79@gmail.com>
2025-11-30 14:10:03 +0200

    fix: next.config - headers only stay

--- next.config.mjs
+++ next.config.mjs
@@ -13,15 +13,40 @@ const nextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [320, 480, 768, 1024, 1280, 1600, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60, // 1 хвилина кешування, можна більше
   },
 
   // Хедери для кешування
- 
-  // Додатково: дозволяємо оптимізацію локальних картинок без проблем на продакшені
-  experimental: {
-    images: {
-      allowFutureImage: true,
-    },
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/fonts/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
