14f5fc08b68ff0dce57f748e8a2dfd143e964d3d
ven0m <yermolenko79@gmail.com>
2025-11-30 14:13:31 +0200

    fix: next.config - remove cache for path

--- next.config.mjs
+++ next.config.mjs
@@ -29,15 +29,6 @@ const nextConfig = {
         ],
       },
       {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
         source: "/fonts/:path*",
         headers: [
           {
