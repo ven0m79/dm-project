59b66fda2b8572ba8cf3c092edbc1111f9f843f3
ven0m <yermolenko79@gmail.com>
2026-02-11 17:09:13 +0200

    fix: binotel autochange phone numbers

--- app/[locale]/components/atoms/scriptsBinotel/scriptsBinotel.tsx
+++ app/[locale]/components/atoms/scriptsBinotel/scriptsBinotel.tsx
@@ -1,53 +1,42 @@
 "use client";
 
+import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-import Script from "next/script";
 
 export default function ClientScriptLoader() {
   const pathname = usePathname();
-  const isSpecialPage = pathname === "/services"; // шлях сторінки, де інший скрипт
 
-  return (
-    <>
-      {!isSpecialPage && (
-        <Script
-          id="binotel-widget-default"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(d, w, s) {
-                var widgetHash = '41zcyas3q551sr3dvq5x';
-                var gcw = d.createElement(s); 
-                gcw.type = 'text/javascript'; 
-                gcw.async = true;
-                gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
-                var sn = d.getElementsByTagName(s)[0]; 
-                sn.parentNode.insertBefore(gcw, sn);
-              })(document, window, 'script');
-            `,
-          }}
-        />
-      )}
+  useEffect(() => {
+    const widgetHash =
+      pathname === "/services"
+        ? "iwuxcf4pbms1bjtplqjy"
+        : "41zcyas3q551sr3dvq5x";
 
-      {isSpecialPage && (
-        <Script
-          id="binotel-widget-special"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(d, w, s) {
-                var widgetHash = 'iwuxcf4pbms1bjtplqjy';
-                var gcw = d.createElement(s); 
-                gcw.type = 'text/javascript'; 
-                gcw.async = true;
-                gcw.src = '//widgets.binotel.com/getcall/widgets/' + widgetHash + '.js';
-                var sn = d.getElementsByTagName(s)[0]; 
-                sn.parentNode.insertBefore(gcw, sn);
-              })(document, window, 'script');
-            `,
-          }}
-        />
-      )}
-    </>
-  );
+    // 1️⃣ Видаляємо попередній скрипт
+    const existingScript = document.querySelector(
+      'script[data-binotel="true"]'
+    );
+    if (existingScript) {
+      existingScript.remove();
+    }
+
+    // 2️⃣ (опціонально) чистимо можливий контейнер віджета
+    const existingWidget = document.querySelector(
+      '[class*="binotel"]'
+    );
+    if (existingWidget) {
+      existingWidget.remove();
+    }
+
+    // 3️⃣ Створюємо новий скрипт
+    const script = document.createElement("script");
+    script.src = `https://widgets.binotel.com/getcall/widgets/${widgetHash}.js`;
+    script.async = true;
+    script.setAttribute("data-binotel", "true");
+
+    document.body.appendChild(script);
+
+  }, [pathname]);
+
+  return null;
 }
