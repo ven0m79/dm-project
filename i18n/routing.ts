159052ae9c5f026fbfc639e6cf3f25e20cd54d81
ven0m <yermolenko79@gmail.com>
2026-02-25 14:50:28 +0200

    fix: first try stabilized default languige

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,9 +2,10 @@ import {defineRouting} from 'next-intl/routing';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['ua', 'en'],
+  locales: ['uk', 'en'],
  
   // Used when no locale matches
-  defaultLocale: 'ua',
+  localeDetection: false,
+  defaultLocale: 'uk',
   localePrefix: "as-needed",
 });
