917833d04046b675052bbdfd989da4f479669b04
ven0m <yermolenko79@gmail.com>
2026-03-26 14:13:40 +0200

    add: redirect for old shares4

--- proxy.ts
+++ proxy.ts
@@ -40,12 +40,12 @@ const redirects: Record<string, string> = {
   "/shares/fabiusplusxl/": "/catalog/sub-catalog/product/1506/?category=anesthesia-and-respiratory-devices",
   "/shares/polaris200/": "/catalog/sub-catalog/product/738/?category=operating-and-examination-lamps",
 
-  "/en/shares/AWD655-2H-V1/": "/en/catalog/sub-catalog/product/476/?category=cleaning-and-desinfecting-equipment",
-  "/en/shares/linea/": "/en/info/aktsii/linea/",
-  "/en/shares/polaris200-2/": "/en/catalog/sub-catalog/product/738/?category=operating-and-examination-lamps",
-  "/en/shares/": "/en/info/aktsii/",
-  "/en/shares/fabiusplusxl/": "/en/catalog/sub-catalog/product/1506/?category=anesthesia-and-respiratory-devices",
-  "/en/shares/polaris200/": "/en/catalog/sub-catalog/product/738/?category=operating-and-examination-lamps",
+  "/en/shares/AWD655-2H-V1/": "/catalog/sub-catalog/product/476/?category=cleaning-and-desinfecting-equipment",
+  "/en/shares/linea/": "/info/aktsii/linea/",
+  "/en/shares/polaris200-2/": "/catalog/sub-catalog/product/738/?category=operating-and-examination-lamps",
+  "/en/shares/": "/info/aktsii/",
+  "/en/shares/fabiusplusxl/": "/catalog/sub-catalog/product/1506/?category=anesthesia-and-respiratory-devices",
+  "/en/shares/polaris200/": "/catalog/sub-catalog/product/738/?category=operating-and-examination-lamps",
 };
 
 const normalizedPath = pathname.endsWith("/") ? pathname : pathname + "/";
