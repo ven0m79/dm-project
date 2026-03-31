3c3e52036760f9c2d53bf5d39b8a65e58d023c1d
ven0m <yermolenko79@gmail.com>
2026-03-31 20:12:07 +0300

    fix: back GTM

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '../../i18n/routing';
 import { cn } from '@app/[locale]/components/molecules/lib/utils';
+import Script from 'next/script';
 //import { Link } from 'i18n/navigation';
 
 type Props = {
@@ -31,6 +32,53 @@ export default async function LocaleLayout({ children, params }: Props) {
 
   return (
     <html lang={locale}>
+      <head>
+        {/* ✅ Обидва скрипти у <head> */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=GT-P3NZ9SN9" />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GT-P3NZ9SN9');`,
+          }}
+        />
+
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17295797148"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-17295797148');
+        `,
+          }}
+        />
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-52H85B3W');
+            `,
+          }}
+        />
+      </head>
       <body className={cn('flex min-h-screen overflow-x-hidden', roboto.variable)}>
         <main className="flex flex-1 flex-col items-center justify-center gap-12 w-full">
           <NextIntlClientProvider locale={locale} messages={messages}>
